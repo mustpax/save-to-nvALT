26bdcd3aea982445c00649e80d60f913d02133be
Mustafa Paksoy <paksoy@gmail.com>
2016-02-03 21:06:17 -0800

    Add IIFE around global.js.

--- global.js
+++ global.js
@@ -1,109 +1,113 @@
-var cmPage = chrome.contextMenus.create({"title": "Create note with page","contexts": ["page"],"onclick": clickstaPage});
-var cmLink = chrome.contextMenus.create({"title": "Create note with selected link","contexts": ["link"],"onclick": clickstaLink});
-var cmSel = chrome.contextMenus.create({"title": "Create note with selected text","contexts": ["selection"],"onclick": clickstaSelection});
-var seltext = null;
+(function() {
+  "use strict";
 
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-	switch (request.message) {
-	case 'setText':
-		window.seltext = request.data
-		break;
+  var cmPage = chrome.contextMenus.create({"title": "Create note with page","contexts": ["page"],"onclick": clickstaPage});
+  var cmLink = chrome.contextMenus.create({"title": "Create note with selected link","contexts": ["link"],"onclick": clickstaLink});
+  var cmSel = chrome.contextMenus.create({"title": "Create note with selected text","contexts": ["selection"],"onclick": clickstaSelection});
+  var seltext = null;
 
-	default:
-		sendResponse({
-			data: 'Invalid arguments'
-		});
-		break;
-	}
-});
+  chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+    switch (request.message) {
+      case 'setText':
+        window.seltext = request.data
+          break;
 
-function prepareLinkUrl(aLink) {
-	return encodeURIComponent(aLink);
-}
+      default:
+        sendResponse({
+          data: 'Invalid arguments'
+        });
+        break;
+    }
+  });
 
-chrome.browserAction.onClicked.addListener(function(theTab) {
-	var theLink = new Object();
-	var theURL = prepareLinkUrl(theTab.url);
-	theLink.url = "nvalt://make/?url=" + theURL + "&title=" + encodeURIComponent(theTab.title);
-	theLink.tab = theTab.id;
-	handle_message(theLink);
-	sendResponse({});
-});
+  function prepareLinkUrl(aLink) {
+    return encodeURIComponent(aLink);
+  }
 
-function clickstaPage(info, tab) {
-	var theLink = new Object();
-	var theURL = prepareLinkUrl(info.pageUrl);
-	theLink.url = "nvalt://make/?url=" + theURL + "&title=" + encodeURIComponent(tab.title);
+  chrome.browserAction.onClicked.addListener(function(theTab) {
+    var theLink = new Object();
+    var theURL = prepareLinkUrl(theTab.url);
+    theLink.url = "nvalt://make/?url=" + theURL + "&title=" + encodeURIComponent(theTab.title);
+    theLink.tab = theTab.id;
+    handle_message(theLink);
+    sendResponse({});
+  });
 
-	theLink.tab = tab.id;
-	handle_message(theLink);
+  function clickstaPage(info, tab) {
+    var theLink = new Object();
+    var theURL = prepareLinkUrl(info.pageUrl);
+    theLink.url = "nvalt://make/?url=" + theURL + "&title=" + encodeURIComponent(tab.title);
 
-}
+    theLink.tab = tab.id;
+    handle_message(theLink);
 
-function clickstaSelection(info, tab) {
-	var theLink = new Object();
-	var title, theBody,
-	theURL=info.pageUrl;
-	if (seltext == null || seltext == '') {
-		theBody = info.selectionText;
-	} else {
-		theBody = seltext;
-	}
-	seltext=null;
-	theBody = theBody.replace(/(\r\n|\n|\n\n|\n\n\n|\r)/gm, "\n");
-	theBody = theBody.replace(/\n/g, "\n");
-	title = titleFromBody(theBody);
-	theBody = encodeURIComponent(theBody);
-	title = encodeURIComponent(title);
-	theURL = encodeURIComponent(theURL);
-	theBody = theURL + "%0D%0D" + theBody;
-	theLink.url = "nvalt://make/?txt=" + theBody + "&title=" + title;
-	theLink.tab = tab.id;
-	handle_message(theLink);
-}
+  }
 
-function titleFromBody(intxt) {
-	var title = intxt;
-	if (title.indexOf('\n') != -1) {
-		title = title.substring(0, title.indexOf('\n'));
-	}
-	if (title.length > 45) {
-		title = title.substring(0, 44);
-	}
-	//	title = title.replace(/\s*$/, '');
-	return title;
-}
+  function clickstaSelection(info, tab) {
+    var theLink = new Object();
+    var title, theBody,
+    theURL=info.pageUrl;
+    if (seltext == null || seltext == '') {
+      theBody = info.selectionText;
+    } else {
+      theBody = seltext;
+    }
+    seltext=null;
+    theBody = theBody.replace(/(\r\n|\n|\n\n|\n\n\n|\r)/gm, "\n");
+    theBody = theBody.replace(/\n/g, "\n");
+    title = titleFromBody(theBody);
+    theBody = encodeURIComponent(theBody);
+    title = encodeURIComponent(title);
+    theURL = encodeURIComponent(theURL);
+    theBody = theURL + "%0D%0D" + theBody;
+    theLink.url = "nvalt://make/?txt=" + theBody + "&title=" + title;
+    theLink.tab = tab.id;
+    handle_message(theLink);
+  }
 
-function clickstaLink(info, tab) {
-	var theLink = new Object();
-	var theURL = prepareLinkUrl(info.linkUrl);
-	theLink.url = "nvalt://make/?url=" + theURL;
-	theLink.tab = tab.id;
+  function titleFromBody(intxt) {
+    var title = intxt;
+    if (title.indexOf('\n') != -1) {
+      title = title.substring(0, title.indexOf('\n'));
+    }
+    if (title.length > 45) {
+      title = title.substring(0, 44);
+    }
+    //	title = title.replace(/\s*$/, '');
+    return title;
+  }
 
-	chrome.tabs.sendRequest(tab.id, {
-		lynx: theLink
-	},
-	function(response) {
-		if (info.selectionText) {
-			info.selectionText = info.selectionText.replace(/(\r\n|\n|\n\n|\n\n\n|\r)/gm, "\n");
-			if (info.selectionText.indexOf('\n') != -1) {
-				info.selectionText = info.selectionText.substring(0, info.selectionText.indexOf('\n'));
-			}
-			if (info.selectionText.length > 35) {
-				info.selectionText = info.selectionText.substring(0, 34);
-			};
-			info.selectionText = info.selectionText.replace(/\s*$/, '');
-			theLink.url = theLink.url + "&title=" + encodeURIComponent(info.selectionText);
-		} else if (response.ttl) {
-			theLink.url = theLink.url + "&title=" + encodeURIComponent(response.ttl);
-		}
-		handle_message(theLink);
-	});
-}
+  function clickstaLink(info, tab) {
+    var theLink = new Object();
+    var theURL = prepareLinkUrl(info.linkUrl);
+    theLink.url = "nvalt://make/?url=" + theURL;
+    theLink.tab = tab.id;
 
-function handle_message(lnx) {
-	var props = new Object();
-	props.url = lnx.url;
-	/*	alert(props.url);*/
-	chrome.tabs.update(lnx.tab, props);
-}
+    chrome.tabs.sendRequest(tab.id, {
+      lynx: theLink
+    },
+    function(response) {
+      if (info.selectionText) {
+        info.selectionText = info.selectionText.replace(/(\r\n|\n|\n\n|\n\n\n|\r)/gm, "\n");
+        if (info.selectionText.indexOf('\n') != -1) {
+          info.selectionText = info.selectionText.substring(0, info.selectionText.indexOf('\n'));
+        }
+        if (info.selectionText.length > 35) {
+          info.selectionText = info.selectionText.substring(0, 34);
+        };
+        info.selectionText = info.selectionText.replace(/\s*$/, '');
+        theLink.url = theLink.url + "&title=" + encodeURIComponent(info.selectionText);
+      } else if (response.ttl) {
+        theLink.url = theLink.url + "&title=" + encodeURIComponent(response.ttl);
+      }
+      handle_message(theLink);
+    });
+  }
+
+  function handle_message(lnx) {
+    var props = new Object();
+    props.url = lnx.url;
+    /*	alert(props.url);*/
+    chrome.tabs.update(lnx.tab, props);
+  }
+})();

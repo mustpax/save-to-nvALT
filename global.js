409d3a2f9aa774d61d6fce0fe3a8147edc933784
Mustafa Paksoy <paksoy@gmail.com>
2016-02-04 20:18:15 -0800

    Rewrite everything!

--- global.js
+++ global.js
@@ -1,109 +1,9 @@
 (function() {
   "use strict";
-
-  var cmPage = chrome.contextMenus.create({"title": "Create note with page","contexts": ["page"],"onclick": clickstaPage});
-  var cmLink = chrome.contextMenus.create({"title": "Create note with selected link","contexts": ["link"],"onclick": clickstaLink});
-  var cmSel = chrome.contextMenus.create({"title": "Create note with selected text","contexts": ["selection"],"onclick": clickstaSelection});
-  var seltext = null;
-
-  chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-    switch (request.message) {
-      case 'setText':
-        window.seltext = request.data
-          break;
-
-      default:
-        sendResponse({
-          data: 'Invalid arguments'
-        });
-        break;
-    }
-  });
-
-  function prepareLinkUrl(aLink) {
-    return encodeURIComponent(aLink);
-  }
-
-  chrome.browserAction.onClicked.addListener(function(tab) {
+  // TODO collapse newlines for selections
+  function savePage(tab) {
     saveToNv({url : tab.url, title: tab.title});
-  });
-
-  function clickstaPage(info, tab) {
-    var theLink = new Object();
-    var theURL = prepareLinkUrl(info.pageUrl);
-    theLink.url = "nvalt://make/?url=" + theURL + "&title=" + encodeURIComponent(tab.title);
-
-    theLink.tab = tab.id;
-    handle_message(theLink);
-
   }
-
-  function clickstaSelection(info, tab) {
-    var theLink = new Object();
-    var title, theBody,
-    theURL=info.pageUrl;
-    if (seltext == null || seltext == '') {
-      theBody = info.selectionText;
-    } else {
-      theBody = seltext;
-    }
-    seltext=null;
-    theBody = theBody.replace(/(\r\n|\n|\n\n|\n\n\n|\r)/gm, "\n");
-    theBody = theBody.replace(/\n/g, "\n");
-    title = titleFromBody(theBody);
-    theBody = encodeURIComponent(theBody);
-    title = encodeURIComponent(title);
-    theURL = encodeURIComponent(theURL);
-    theBody = theURL + "%0D%0D" + theBody;
-    theLink.url = "nvalt://make/?txt=" + theBody + "&title=" + title;
-    theLink.tab = tab.id;
-    handle_message(theLink);
-  }
-
-  function titleFromBody(intxt) {
-    var title = intxt;
-    if (title.indexOf('\n') != -1) {
-      title = title.substring(0, title.indexOf('\n'));
-    }
-    if (title.length > 45) {
-      title = title.substring(0, 44);
-    }
-    //	title = title.replace(/\s*$/, '');
-    return title;
-  }
-
-  function clickstaLink(info, tab) {
-    var theLink = new Object();
-    var theURL = prepareLinkUrl(info.linkUrl);
-    theLink.url = "nvalt://make/?url=" + theURL;
-    theLink.tab = tab.id;
-
-    chrome.tabs.sendRequest(tab.id, {
-      lynx: theLink
-    },
-    function(response) {
-      if (info.selectionText) {
-        info.selectionText = info.selectionText.replace(/(\r\n|\n|\n\n|\n\n\n|\r)/gm, "\n");
-        if (info.selectionText.indexOf('\n') != -1) {
-          info.selectionText = info.selectionText.substring(0, info.selectionText.indexOf('\n'));
-        }
-        if (info.selectionText.length > 35) {
-          info.selectionText = info.selectionText.substring(0, 34);
-        };
-        info.selectionText = info.selectionText.replace(/\s*$/, '');
-        theLink.url = theLink.url + "&title=" + encodeURIComponent(info.selectionText);
-      } else if (response.ttl) {
-        theLink.url = theLink.url + "&title=" + encodeURIComponent(response.ttl);
-      }
-      handle_message(theLink);
-    });
-  }
-
-  function handle_message(lnx) {
-    var props = new Object();
-    props.url = lnx.url;
-    /*	alert(props.url);*/
-    chrome.tabs.update(lnx.tab, props);
 
   function saveToNv(params) {
     var base = "nvalt://make/";
@@ -114,4 +14,38 @@
     var url = [base, paramStr].join('?');
     chrome.tabs.update(null, {url: url});
   }
+
+  // Save whole page when extension button in toolbar is clicked
+  chrome.browserAction.onClicked.addListener(savePage);
+
+  // Context menu options
+  chrome.contextMenus.create({
+    "title": "Save note with page",
+    "contexts": ["page"],
+    "onclick": function(info, tab) {
+      savePage(tab);
+    }
+  });
+
+  chrome.contextMenus.create({
+    "title": "Save note with selected link",
+    "contexts": ["link"],
+    "onclick": function(info, tab) {
+      saveToNv({
+        title: info.selectionText,
+        url: info.linkUrl
+      });
+    }
+  });
+
+  chrome.contextMenus.create({
+    "title": "Save note with selected text",
+    "contexts": ["selection"],
+    "onclick": function(info, tab) {
+      saveToNv({
+        title: tab.title,
+        txt: info.pageUrl + '\n\n' + info.selectionText
+      });
+    }
+  });
 })();
